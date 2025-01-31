9af2856085f0f99678d9063d4f5ba7994d6cd097
jesse-barbosa <barbosajesse419@gmail.com>
2025-01-31 12:54:57 -0300

    Redesign welcome page with hero section and simplified layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,69 +10,54 @@
         <!-- Styles / Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="bg-[#1D1D1D] text-white/50">
-        <div class="bg-black text-white/50">
-            <header class="flex justify-between items-center gap-2 p-4">
+    <body class="bg-black text-white">
+        <div class="flex justify-center ">
+            <header class="fixed w-full z-50 top-0 flex justify-between max-w-7xl mx-auto items-center gap-2 p-4">
                 <div class="flex lg:justify-center lg:col-start-2">
                     <img src="{{ asset('/assets/images/logo.png') }}" alt="TechMix" height="64" width="64">
                 </div>
                 <nav class="-mx-3 flex flex-1 justify-end">
                     <a
-                        href="{{ route('login') }}"
-                        class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-white/80 focus:outline-none focus-visible:ring-white"
+                        href="{{ url('/dashboard') }}"
+                        class="rounded-md px-3 py-2 ring-1 ring-transparent transition hover:text-white/80 focus:outline-none focus-visible:ring-white"
                     >
                     Entrar
                     </a>
 
                     <a
-                    href="{{ route('register') }}"
-                        class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-white/80 focus:outline-none focus-visible:ring-white"
+                        href="{{ url('/register') }}"
+                        class="rounded-md px-3 py-2 ring-1 ring-transparent transition hover:text-white/80 focus:outline-none focus-visible:ring-white"
                     >
                     Cadastrar
                     </a>
                 </nav>
             </header>
-
-            <main class="mt-6">
-                <section class="py-5 text-white">
-                    <div class="max-w-7xl mx-auto text-center">
-                        <h2 class="text-2xl font-bold">Sobre Nós</h2>
-                        <p class="mt-2">Na TechMix, estamos comprometidos em trazer as inovações tecnológicas mais recentes para o seu dia a dia. Nossa missão é conectar você ao futuro.</p>
-                    </div>
-                </section>
-
-                <section class="py-5">
-                    <div class="max-w-7xl mx-auto">
-                        <div class="flex flex-col md:flex-row">
-                            <div class="md:w-1/2">
-                            <img src="img/innovation.jpg" class="w-full h-auto" alt="Inovação">
-                            </div>
-                            <div class="md:w-1/2 mt-4 md:mt-0 md:ml-4">
-                                <h2 class="text-2xl font-bold">Inovações Recentes</h2>
-                                <p class="mt-2">Descubra como nossas soluções estão transformando indústrias e melhorando vidas. Desde inteligência artificial até a Internet das Coisas, estamos na vanguarda da tecnologia.</p>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-
-                <section class="py-5 text-white">
-                    <div class="max-w-7xl mx-auto text-center">
-                        <h2 class="text-2xl font-bold">Entre em Contato</h2>
-                        <p class="mt-2">Quer saber mais sobre nossos produtos e serviços? Entre em contato conosco e descubra como podemos ajudar você a alcançar seus objetivos tecnológicos.</p>
-                        <button class="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 transition">Fale Conosco</button>
-                    </div>
-                </section>
-            </main>
-
-        <footer class="flex flex-col items-center justify-center py-16 text-center text-sm text-gray-300">
-            TechMix &copy; {{ date('Y') }}. Todos os direitos reservados.
-            <a
-            href="{{ url('/dashboard') }}"
-            class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-white/80 focus:outline-none focus-visible:ring-white"
-            >
-                Dashboard
-            </a>
-        </footer>
         </div>
+        <main>
+
+            <!-- Hero -->
+            <section class="h-screen flex flex-col items-center justify-center relative overflow-hidden">
+                <div class="absolute inset-0 bg-black">
+                    <div class="checkered-background"></div>
+                </div>
+                <div class="max-w-4xl mx-auto text-center relative z-10">
+                    <h1 class="text-5xl xl:text-6xl font-bold">Transforme suas <span class="text-yellow-500">ideias</span> em realidade com seu próprio <span class="text-yellow-500">e-commerce</span></h1>
+                    <p class="my-4 text-neutral-400">Divulgue seu catálogo, converse com clientes, faça Marketing e impulsione suas vendas</p>
+                </div>
+            </section>
+
+
+            <section class="py-5">
+                <div class="max-w-7xl mx-auto text-center">
+                    <h2 class="text-2xl font-bold">Crie sua Loja</h2>
+                    <p class="mt-2">Crie sua loja em poucos minutos e comece a divulgar seus produtos de forma simples e rápida.</p>
+                    <button class="mt-4 px-4 py-2 bg-blue-500 rounded-md hover:bg-blue-600 transition">Fale Conosco</button>
+                </div>
+            </section>
+        </main>
+
+        <footer class="flex flex-col items-center justify-center py-12 text-center text-sm text-gray-300">
+            TechMix &copy; {{ date('Y') }}. Todos os direitos reservados.
+        </footer>
     </body>
 </html>
