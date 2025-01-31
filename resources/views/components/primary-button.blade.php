fefc834c2816092315f5eee1b59ce92ce980268f
jesse-barbosa <barbosajesse419@gmail.com>
2025-01-31 12:53:58 -0300

    class 'w-full' in primary-button

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'px-4 py-2 bg-yellow-500 hover:bg-yellow-400 font-bold focus:outline-none transition']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'w-full px-4 py-2 bg-yellow-500 hover:bg-yellow-400 font-bold focus:outline-none transition']) }}>
     {{ $slot }}
 </button>
