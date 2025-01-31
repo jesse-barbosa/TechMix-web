f423c7c44360c90768b47e2e2014570d922dfed5
jesse-barbosa <barbosajesse419@gmail.com>
2025-01-31 14:21:40 -0300

    Defining fixed size for favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>TechMix</title>
-        <link rel="icon" type="image/x-icon" href="{{ asset('/assets/images/logo.png') }}">
+        <link rel="icon" type="image/x-icon" href="{{ asset('/assets/images/logo.png ') }}" sizes="32x32" />
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
