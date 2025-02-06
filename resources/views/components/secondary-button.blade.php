5cad0c39ed09c6b138018cb716cc376ff7ac3753
jesse-barbosa <barbosajesse419@gmail.com>
2025-02-06 08:23:33 -0300

    Removing indigo focus in secondary button

--- resources/views/components/secondary-button.blade.php
+++ resources/views/components/secondary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'button', 'class' => 'px-4 py-2 bg-neutral-800 border border-neutral-500 text-neutral-300 hover:bg-neutral-700 focus:ring-2 focus:ring-indigo-500 disabled:opacity-25 transition']) }}>
+<button {{ $attributes->merge(['type' => 'button', 'class' => 'px-4 py-2 bg-neutral-800 border border-neutral-500 text-neutral-300 hover:bg-neutral-700 transition']) }}>
     {{ $slot }}
 </button>
